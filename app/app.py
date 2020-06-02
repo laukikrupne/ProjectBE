2273668487a3281b6591fa3f523fa84e41795304
laukikrupne <laukikrupne11@gmail.com>
2020-06-02 09:44:07 +0530

    select operator

--- app/app.py
+++ app/app.py
@@ -319,7 +319,7 @@ def predict():
             y = r.json()
             zzz = y['Deficiency']
             #zzzz = inf_dict[zzz]
-            zzzz = Deficiency.select([deficiency]).where(deficiency.columns.deficiency == zzz)
+            zzzz = Deficiency.select().where(deficiency.columns.deficiency == zzz)
             return render_template('def.html', string = zzzz)
     else:
         return redirect(url_for('index'))
